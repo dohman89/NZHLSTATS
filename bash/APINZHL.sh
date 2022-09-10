3bf1b98d8557d9cd9856d837e2e7c2ac38832c5d
dohman89 <dohman89@gmail.com>
2022-09-09 20:15:57 -0500

    next week

--- bash/APINZHL.sh
+++ bash/APINZHL.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=79718&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/79718.json
-curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=101458&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/101458.json
-curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=149878&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/149878.json 
-curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=152212&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/152212.json
-curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=145078&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/145078.json 
-curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=150265&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/150265.json 
-curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=155815&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/155815.json
-curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=150730&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/150730.json
+#change the number inside the single qoutes to the clubid found by going to https://www.ea.com/games/nhl/nhl-22/pro-clubs/rankings searching for the club, and going to its page. number is located ub the URL afte ?clubid=.
+team1='101458'
+team2='145078'
+team3='149878'
+team4='150265'
+team5='150730'
+team6='152212'
+team7='155815'
+team8='79718'
+
+
+curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=$team1&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/$team1.json
+
+curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=$team2&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/$team2.json
+
+curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=$team3&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/$team3.json
+
+curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=$team4&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/$team4.json
+
+curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=$team5&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/$team5.json
+
+curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=$team6&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/$team6.json
+
+curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=$team7&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/$team7.json
+
+curl "https://proclubs.ea.com/api/nhl/clubs/matches?clubIds=$team8&platform=xbox-series-xs&matchType=club_private" -A "Gecko/20100101" >> /home/joe/NZHLSTATS/STATSJSON/$team8.json
+
