e2ed52a9395bd279c26a25dc597c7943289a923c
dohman89 <dohman89@gmail.com>
2022-09-26 03:59:20 -0500

    update

--- bash/Run.sh
+++ bash/Run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source "/home/joe/NZHLSTATS/bash/APINZHL.sh" &&
-source "/home/joe/NZHLSTATS/bash/tocsv.sh"
+source "/home/joe/NZHLSTATS/bash/tocsv.sh" &&
+source "/home/joe/NZHLSTATS/AUTOUPDATE.sh"
